62950b1b95951494ee8b3fb889635cbd96b2a2a0
Akumu-1 <barelyonthisearth421@gmail.com>
2025-06-26 00:41:55 +0100

    fixed various bugs

--- script7.js
+++ script7.js
@@ -1,109 +1,69 @@
 const disp = document.getElementById("myH1");
 let result;
 let variable = '';
-let op = 0,a=0,b=0,c=0;
+let op = 0;
 
 function operation(op,a,b){
     switch (op) {
         case 0:
             return a;
-            break;
         case 1:
             return a+b;
-            break;
         case 2:
             return a-b;
-            break;
         case 3:
             return a*b;
-            break;
         case 4:
             if(b !== 0){
                 return a/b;
             }
             else {return 'ERROR';}
-            break;
     }
 }
 
-function prb(){
-    c++;
-    op === 0 ? a = Number(variable): b = Number(variable);
-    c >= 2 ? result=operation(op,result,b): result=operation(op,a,b);
-    variable='';
-    disp.innerText = result;
-    if(result === 'ERROR'){
-       result = 0,op = 0,a = 0,b = 0,c = 0;
+function prb() {
+    if (variable !== '') {
+        const num = Number(variable);
+        if (op === 0) {
+            result = num;
+        } else {
+            result = operation(op, result, num);
+        }
+
+        if (result === 'ERROR') {
+            resetAll();
+            disp.innerText = 'ERROR';
+        } else {
+            disp.innerText = result;
+        }
+
+        variable = '';
     }
 }
 
-document.getElementById("n1").onclick = function(){
-    variable += "1";
-    disp.innerText = variable;
+function resetAll() {
+    variable = '';
+    op = 0;
+    result = 0;
 }
 
-document.getElementById("n2").onclick = function(){
-    variable += "2";
-    disp.innerText = variable;
-}
-
-document.getElementById("n3").onclick = function(){
-    variable += "3";
-    disp.innerText = variable;
-}
-
-document.getElementById("n4").onclick = function(){
-    variable += "4";
-    disp.innerText = variable;
-}
-
-document.getElementById("n5").onclick = function(){
-    variable += "5";
-    disp.innerText = variable;
-}
-
-document.getElementById("n6").onclick = function(){
-    variable += "6";
-    disp.innerText = variable;
-}
-
-document.getElementById("n7").onclick = function(){
-    variable += "7";
-    disp.innerText = variable;
-}
-
-document.getElementById("n8").onclick = function(){
-    variable += "8";
-    disp.innerText = variable;
-}
-
-document.getElementById("n9").onclick = function(){
-    variable += "9";
-    disp.innerText = variable;
-}
-
-document.getElementById("n0").onclick = function(){
-    variable += "0";
-    disp.innerText = variable;
+for (let i = 0; i <= 9; i++) {
+    document.getElementById("n" + i).onclick = function () {
+        variable += i;
+        disp.innerText = variable;
+    }
 }
 
 document.getElementById("dot").onclick = function(){
-    if((variable.indexOf(".")) == -1){
+    if((variable.indexOf(".")) === -1){
         variable += ".";
     }
     disp.innerText = variable;
 }
 
 document.getElementById("eq").onclick = function(){
-    if((variable !== '') && (op !== 0)){
-        op === 0 ? a = Number(variable): b = Number(variable);
-        c >= 2 ? result=operation(op,result,b):     result=operation(op,a,b);
-        disp.innerText = result;
-        if(result === 'ERROR'){
-            result = 0,c=0;
-        }
-        variable = '';
-    }
+    prb();
+    op = 0;
 }
 
 document.getElementById("add").onclick = function(){
